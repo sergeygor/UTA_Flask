f615a8f66df1a628b2cb2bd911814a07fc279585
sergeygor <sgordeev68git@gmail.com>
2019-11-30 06:06:24 -0800

    Accessing data via request and response objects added

--- enrollment/application/routes.py
+++ enrollment/application/routes.py
@@ -1,5 +1,14 @@
 from application import app
-from flask import render_template, request
+from flask import render_template, request, json, Response
+
+courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP",
+ "credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1",
+"description":"Intro to Java Programming","credits":4,"term":"Spring"},
+ {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming",
+ "credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1",
+ "description":"Intro to Angular","credits":3,"term":"Fall, Spring"},
+ {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming",
+ "credits":4,"term":"Fall"}]
 
 
 @app.route("/")
@@ -12,18 +21,10 @@ def index():
 def login():
     return render_template("login.html", login = True)
 
-@app.route("/courses")
-def courses():
-    courseData = [{"courseID":"1111","title":"PHP 101","description":"Intro to PHP",
-     "credits":3,"term":"Fall, Spring"}, {"courseID":"2222","title":"Java 1",
-    "description":"Intro to Java Programming","credits":4,"term":"Spring"},
-     {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming",
-     "credits":3,"term":"Fall"}, {"courseID":"4444","title":"Angular 1",
-     "description":"Intro to Angular","credits":3,"term":"Fall, Spring"},
-     {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming",
-     "credits":4,"term":"Fall"}]
-    print(courseData[0]["title"])
-    return render_template("courses.html", courseData = courseData, courses = True)
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term = "Spring 2019"):
+    return render_template("courses.html", courseData = courseData, courses = True, term = term)
 
 @app.route("/register")
 def register():
@@ -35,3 +36,13 @@ def enrollment():
     title = request.form.get('title')
     term = request.form.get('term')
     return render_template("enrollment.html", enrollment = True, data={"id":id, "title":title,"term":term})
+
+@app.route("/api/")
+@app.route("/api/<idx>")
+def api(idx=None):
+    if(idx == None):
+        jdata = coruseData
+    else:
+        jdata = courseData[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype = "application/json")
