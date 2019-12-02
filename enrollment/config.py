8de9e1cb487eb059ed473da21e7c655c9e6fce55
sergeygor <sgordeev68git@gmail.com>
2019-12-02 15:42:46 -0800

    Setting upp database

--- enrollment/config.py
+++ enrollment/config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config(object):
-    SECRET_KY = os.environ.get('SECRET_KEY') or "secret_string"
-    
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "secret_string"
+
+    MONGODB_SETTINGS = {'db' : 'UTA_Enrolment',
+                'host': 'mongodb://localhost:27017/UTA_Enrolment'
+    }
